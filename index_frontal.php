95522760c8ac29ddc840776a02b6a9209668662f
raghast <abardy26@gmail.com>
2017-03-28 17:46:07 +0200

    Importation du controller frontal
    Quasiment identique à celui de mon blog

---
+++ index_frontal.php
@@ -0,0 +1,25 @@
+<?php
+
+// La page init.php n'est pas encore définie mais elle le sera dans un futur proche !
+require_once 'Utils/init.php';
+
+// Variable controller avec l'acceuil du quizz comme valeur par défault
+$controller = 'accueil_quizz';
+
+if (!empty($_GET['controller'])) {
+    $controller = $_GET['controller'];
+}
+
+// Création du chemin à partir de la variable GET précédente
+$path = 'Controller/'.$controller.'.php';
+
+// Si le chemin existe, lien vers celui-ci sinon lien vers la page error 404
+if (file_exists($path)) 
+{
+    require_once $path;
+} 
+else 
+{
+    require_once 'Vue/404.php';
+}
+
