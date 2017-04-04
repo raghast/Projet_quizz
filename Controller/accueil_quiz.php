b9649170da28b0234868fb740359d809e0cbed11
raghast <abardy26@gmail.com>
2017-04-04 17:33:00 +0200

    Modification controller accueil_quiz
    Changement du controller accueil quiz du au passage du manager à une fonction d'importation de la BDD.

--- Controller/accueil_quiz.php
+++ Controller/accueil_quiz.php
@@ -1,18 +1,17 @@
 <?php
 
 // Importations des classes
-require_once 'Modele/quiz.php';
-require_once 'Modele/quiz_manager.php';
+require_once 'Modele/question.php';
+require_once 'Modele/fonctions_quiz.php';
+require_once 'Modele/reponse.php';
+require_once 'Modele/bonne_reponse.php';
 
 // Initialisation des variables
-$db = new PDO('mysql:host=localhost;dbname=projet_quiz', 'root', '');
-$manager = new QuizManager($db);
+$questions = importation_questions($_GET['id_quiz']);
 
-// Initialisation de la numérotation des quiz
+// Initialisation de la numérotation des questions
 $j=0;
 
+
 // Importation de la vue
-require_once 'Vue/vue_accueil_quiz.php';
-
-
-
+require_once 'Vue/vue_quiz.php';
