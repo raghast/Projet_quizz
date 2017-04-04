ff0c395016c332b95c75d8db8ece928a5a1d0796
raghast <abardy26@gmail.com>
2017-04-04 16:33:32 +0200

    Importation de fonctions
    J'ai crée des fonctions pour importer les données de la BDD

---
+++ Modele/fonctions_quiz.php
@@ -0,0 +1,34 @@
+<?php
+
+function importation_questions($id)
+{
+    global $bdd;
+    $req = $bdd->prepare('SELECT id_question, question, id_quiz FROM questions_quiz WHERE id_quiz = :id');
+    $req->bindValue(':id', $id, PDO::PARAM_INT);
+    $req->execute();
+    $donnees = $req->fetchAll();
+
+    return $donnees;
+}
+
+function importation_reponses($id)
+{
+    global $bdd;
+    $req = $bdd->prepare('SELECT id_reponse, reponse, id_question FROM reponses_quiz WHERE id_question = :id');
+    $req->bindValue(':id', $id, PDO::PARAM_INT);
+    $req->execute();
+    $donnees = $req->fetchAll();
+
+    return $donnees;
+}
+
+function importation_br($id)
+{
+    global $bdd;
+    $req = $bdd->prepare('SELECT id_br, bonne_reponse, id_question FROM bonne_reponse WHERE id_question = :id');
+    $req->bindValue(':id', $id, PDO::PARAM_INT);
+    $req->execute();
+    $donnees = $req->fetchAll();
+
+    return $donnees;
+}
