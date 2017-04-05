6dc101cab9930c67a5a5bac7066709aeb3d2733d
raghast <abardy26@gmail.com>
2017-04-05 19:21:09 +0200

    Quelques modifications
    Suppression de la classe BReponse dont on ne se sert pas dans cette page

--- Controller/page_quiz.php
+++ Controller/page_quiz.php
@@ -1,10 +1,9 @@
 <?php
 
-// Importations des classes
+// Importations des classes et fonctions
 require_once 'Modele/question.php';
 require_once 'Modele/fonctions_quiz.php';
 require_once 'Modele/reponse.php';
-require_once 'Modele/bonne_reponse.php';
 
 // Initialisation des variables
 $questions = importation_questions($_GET['id_quiz']);
