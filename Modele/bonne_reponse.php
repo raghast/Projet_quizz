709ba2782b9ae799a6b665f2081ffdb9cc1f1aaf
raghast <abardy26@gmail.com>
2017-04-10 20:38:48 +0200

    Modication de la classe Bonne_reponse
    Ajout du getter question(), qui permet d'afficher la question auquel est rattacher la bonne réponse (évite d'avoir a importer la classe question en plus pour afficher la question.
    Modification des notations.

--- Modele/bonne_reponse.php
+++ Modele/bonne_reponse.php
@@ -4,7 +4,8 @@ class BReponse
 {
   private $_id;
   private $_bonne_reponse;
-  private $_id_question;
+  private $_question;
+  private $_id_question_br;
   
   public function __construct($value = array())
   {
@@ -24,9 +25,14 @@ class BReponse
     return $this->_bonne_reponse;
   }
 
+  public function question()
+  {
+    return $this->_question;
+  }
+
   // Liste des setters
   
-  public function setId($id)
+  public function setId_Br($id)
   {
     // On convertit l'argument en nombre entier.
     // Si c'en était déjà un, rien ne changera.
@@ -41,7 +47,16 @@ class BReponse
     }
   }
   
-  public function setBReponse($brep)
+  public function setQuestion($question)
+  {
+    // On vérifie qu'il s'agit bien d'une chaîne de caractères.
+    if (is_string($question))
+    {
+      $this->_question = $question;
+    }
+  }
+
+  public function setBonne_Reponse($brep)
   {
     // On vérifie qu'il s'agit bien d'une chaîne de caractères.
     if (is_string($brep))
@@ -50,18 +65,18 @@ class BReponse
     }
   }
 
-  public function setIdQuestion($id_question)
+  public function setId_Question_Br($id_question_br)
   {
      // On convertit l'argument en nombre entier.
     // Si c'en était déjà un, rien ne changera.
     // Sinon, la conversion donnera le nombre 0 (à quelques exceptions près, mais rien d'important ici).
-    $id_question = (int) $id_question;
+    $id_question_br = (int) $id_question_br;
     
     // On vérifie ensuite si ce nombre est bien strictement positif.
-    if ($id_question > 0)
+    if ($id_question_br > 0)
     {
       // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
-      $this->_id_question = $id_question;
+      $this->_id_question_br = $id_question_br;
     }
   }
 
