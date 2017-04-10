f501aeb22c4846bbfc8bbc604859764f031c88ab
raghast <abardy26@gmail.com>
2017-04-10 14:44:50 +0200

    Ajout du dossiers Utils
    Ajout d'un dossier Utils avec une page init.php pour les éléments qui concernent l'ensemble des pages. Ici il n'y a que la connection à la BDD pour le moment

---
+++ Utils/init.php
@@ -0,0 +1,13 @@
+<?php
+
+try
+			{
+   				 // On se connecte à MySQL
+    			$bdd = new PDO('mysql:host=localhost;dbname=projet_quiz;charset=utf8', 'root', '',
+    			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+			}
+			catch(Exception $e)
+			{
+    			// En cas d'erreur, on affiche un message et on arrête tout
+       			die('Erreur : '.$e->getMessage());
+			}
