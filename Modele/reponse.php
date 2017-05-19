161779da7399f070af32724d65820c764632b14f
raghast <abardy26@gmail.com>
2017-05-19 18:16:52 +0200

    Modification classe Reponse
    La classe a été modifiée de sorte à ressembler à l'architecture des autres classes. Rajout de l'attribut state, pour savoir quelle est la bonne réponse (0 mauvaise 1 bonne et pour la suite 1-2-3 .. pour l'ordre de réponse). Suppression de l'attribut Question_Id qui ne sert à rien ici.

--- Modele/reponse.php
+++ Modele/reponse.php
@@ -2,82 +2,77 @@
 
 class Reponse
 {
-  private $_id;
-  private $_reponse;
-  private $_id_question;
-  
-  public function __construct($value = array())
-  {
-      if(!empty($value))
-      $this->hydrate($value);
-  }
+  private $id;
+  private $reponse;
+  private $state;
 
-  // Liste des getters
-  
-  public function id()
-  {
-    return $this->_id;
-  }
-  
-  public function reponse()
-  {
-    return $this->_reponse;
-  }
+    public function __construct($data)
+    {
+        $this->hydrate($data);
+    }
 
-  // Liste des setters
-  
-  public function setId($id)
-  {
-    // On convertit l'argument en nombre entier.
-    // Si c'en était déjà un, rien ne changera.
-    // Sinon, la conversion donnera le nombre 0 (à quelques exceptions près, mais rien d'important ici).
-    $id = (int) $id;
+    // Liste des getters
+
+    public function getId() 
+    {
+        return $this->id;
+    }
+
+    public function getReponse() 
+    {
+        return $this->reponse;
+    }
+
+    public function getState() 
+    {
+        return $this->state;
+    }
+
+    // Liste des setters
+
+    public function setId($id)
+    {
+        // On convertit l'argument en nombre entier.
+        // Si c'en était déjà un, rien ne changera.
+        // Sinon, la conversion donnera le nombre 0 (à quelques exceptions près, mais rien d'important ici).
+        $id = (int) $id;
     
-    // On vérifie ensuite si ce nombre est bien strictement positif.
-    if ($id > 0)
-    {
-      // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
-      $this->_id = $id;
+        // On vérifie ensuite si ce nombre est bien strictement positif.
+        if ($id > 0)
+        {
+          // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
+          $this->id = $id;
+        }
     }
-  }
-  
-  public function setReponse($reponse)
-  {
-    // On vérifie qu'il s'agit bien d'une chaîne de caractères.
-    if (is_string($reponse))
-    {
-      $this->_reponse = $reponse;
-    }
-  }
 
-  public function setIdQuestion($id_question)
-  {
-     // On convertit l'argument en nombre entier.
-    // Si c'en était déjà un, rien ne changera.
-    // Sinon, la conversion donnera le nombre 0 (à quelques exceptions près, mais rien d'important ici).
-    $id_question = (int) $id_question;
+    public function setReponse($reponse)
+    {
+        // On vérifie qu'il s'agit bien d'une chaîne de caractères.
+        if (is_string($reponse))
+        {
+          $this->reponse = $reponse;
+        }
+    }
+
+    public function setState($state)
+    {
+        // On convertit l'argument en nombre entier.
+        // Si c'en était déjà un, rien ne changera.
+        // Sinon, la conversion donnera le nombre 0 (à quelques exceptions près, mais rien d'important ici).
+        $state = (int) $state;
     
-    // On vérifie ensuite si ce nombre est bien strictement positif.
-    if ($id_question > 0)
-    {
-      // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
-      $this->_id_question = $id_question;
+        // On vérifie ensuite si ce nombre est bien strictement positif.
+        if ($state > 0)
+        {
+          // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
+          $this->state = $state;
+        }
     }
-  }
 
-  public function hydrate(array $donnees)
-  {
-  foreach ($donnees as $key => $value)
-  {
-    // On récupère le nom du setter correspondant à l'attribut.
-    $method = 'set'.ucfirst($key);
-        
-    // Si le setter correspondant existe.
-    if (method_exists($this, $method))
+    public function hydrate($data)
     {
-      // On appelle le setter.
-      $this->$method($value);
+      $this->setId($data['Reponse_Id']);
+      $this->setReponse($data['Reponse_Reponse']);
+      $this->setState($data['Reponse_State']);
     }
-  }
-  }
 }
