842ce2616af40bd440c7b816903a818a39e6619f
raghast <abardy26@gmail.com>
2017-05-29 14:46:16 +0200

    Commentaires + Artchitecture
    Rajout de commentaires, et passage à l'architecture semblable à page_quiz, sauf que la fonction importation_br ne récupère que les bonnes réponses. Les 3 fonctions du début sont expliquées en commentaire ou dans le précédent commit.

--- Controller/affichage_resultat.php
+++ Controller/affichage_resultat.php
@@ -1,10 +1,31 @@
 <?php
 
+// Importations des classes et fonctions
+require_once 'Modele/quiz.php';
+require_once 'Modele/question.php';
 require_once 'Modele/fonctions_quiz.php';
-require_once 'Modele/bonne_reponse.php';
+require_once 'Modele/reponse.php';
 
-// Initialisation des variables
-$bonnes_reponses = importation_br($_GET['id_quiz']);
+
+// Fonctions de vérification de la page (pour la variable $_GET['id_quiz'], et Is_autorized pour savoir si l'utilisateur à déjà fais le quiz)
+check_variable();
+check_id($_GET['id_quiz']);
+Is_autorized($_GET['id_quiz']);
+
+// Même système que pour le controller page_quiz mais importation_br importe un quiz, ses questions et la bonne réponse pour chaque question
+$datas = importation_br($_GET['id_quiz']);
+$quizs = [];
+foreach ($datas as $data) 
+{
+	$quiz = new Quiz($data);
+	if (isset($quizs[$quiz->getId()])) 
+	{
+       $quizs[$quiz->getId()]->fusion($quiz);
+    } else 
+    {
+       $quizs[$quiz->getId()] = $quiz;
+    }
+}
 
 // Initialisation de la numérotation des questions
 $j=0;
