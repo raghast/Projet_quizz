28c116aee9f584816f13726660fb4a3b1645d5b7
raghast <abardy26@gmail.com>
2017-05-29 19:31:03 +0200

    Rajout de fonctions
    Importation_br : Importation du quiz, ses questions et la bonne réponse associée
    is_autorized : Fonction qui vérifie si l'utilisateur a déjà fait le quiz
    check_id : Vérifie que l'id en variable correspond bien à un quiz existant
    remplissage : Vérifie que le quiz a été rempli entièrement
    check_variable : Vérifie que la variable $_GET['id_quiz'] existe pour qu'elle soit utilisée dans la fonction check_id.

--- Modele/fonctions_quiz.php
+++ Modele/fonctions_quiz.php
@@ -19,3 +19,59 @@ function importation_tout($id)
 
     return $donnees;
 }
+
+function importation_br($id)
+{
+    global $bdd;
+    $req = $bdd->prepare('SELECT * FROM quiz qz INNER JOIN questions qu ON qz.Quiz_Id = qu.Quiz_ID INNER JOIN reponses rep ON qu.Question_Id = rep.Question_Id WHERE qz.Quiz_Id = :id AND rep.Reponse_State=1');
+    $req->bindValue(':id', $id, PDO::PARAM_INT);
+    $req->execute();
+    $donnees = $req->fetchAll();
+
+    return $donnees;
+}
+
+function is_autorized($Quiz_Id)
+{
+    if (isset($_SESSION['Quiz_done'][$Quiz_Id])) 
+    {
+        $message = 'Vous avez déjà effectué ce quiz !';
+        addFlash($message);
+        header('Location: ?controller=accueil_quiz');
+        die;
+    }
+}
+
+function check_id($id)
+{
+        global $bdd;
+        $req = $bdd->prepare('SELECT Quiz_Id FROM quiz WHERE Quiz_Id = :id_quiz');
+        $req->bindparam('id_quiz', $id, PDO::PARAM_INT);
+        $req->execute();
+        $resultat = $req->fetch();
+        if (empty($resultat)) 
+        {
+            require_once 'Vue/404.php';
+            die;
+        }       
+}
+
+function remplissage($reponse, $id_quiz)
+{
+    if (empty($reponse)) 
+    {
+            $message = 'Veuillez remplir tout le quiz';
+            addFlash($message);
+            header('Location: ?controller=page_quiz&&id_quiz='.$id_quiz);
+            die;
+    }
+}
+
+function check_variable()
+{
+    if (!isset($_GET['id_quiz'])) 
+    {
+        require_once 'Vue/404.php';
+        die;
+    }
+}
