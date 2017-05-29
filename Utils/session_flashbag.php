0026f79548efa38fd82bbd725464e878815992dc
raghast <abardy26@gmail.com>
2017-05-29 20:12:27 +0200

    Ajout de commentaires
    Ajout de commentaires sur les fonctions flashbag

--- Utils/session_flashbag.php
+++ Utils/session_flashbag.php
@@ -2,19 +2,19 @@
 
 session_start();
 
+// Fonction qui ajoute un message au flashbag
 function addFlash($message) 
 { 
     $_SESSION['flashbag'] = $message;
 }
 
-// Fonction qui renvois le message 'succés'
-
+// Fonction qui renvois le message ajouté
 function getFlash() 
 {
 	return ($_SESSION['flashbag']);
-    unset($_SESSION['flashbag']);
 }
 
+// Fonction qui affiche le message si la variable $_SESSION['flashbag'] n'est pas vide et qui la vide ensuite
 function verifFlash()
 {
 	if(!empty($_SESSION['flashbag']))
