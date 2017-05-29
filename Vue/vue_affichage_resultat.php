7282bfc7edefb35b211e209f8af3602def2485fd
raghast <abardy26@gmail.com>
2017-05-29 20:07:25 +0200

    Nouvelle vue_affichage_résultat + Coms
    Nouvelle vue due à la nouvelle architecture.
    Nous partons d'un tableau contenant le quiz, ses questions et les bonnes réponses associées.
    Par le même principe que pour la vue du quiz, nous balayons d'abord les quizs puis les questions (en différenciant les 2 cas de questions !) puis enfin les réponses. Et pratiquement comme dans l'étape 1 du projet nous regardons si la (les) réponse(s) choisis par l'utilisateur correspond(ent) aux réponses de la base de données créant ainsi 2 cas différents supplémentaires.
    Des commentaires sont aussi ajoutés pour mieux comprendre le fonctionnement

--- Vue/vue_affichage_resultat.php
+++ Vue/vue_affichage_resultat.php
@@ -17,50 +17,103 @@
   <body>
   <div class="container">
       <header class="jumbotron">
-          <h1>Faites un Quiz en ligne !</h1></br></br>
-          <h2>Résultats sur le  : <?= $_GET['nom'] ?></h2>
+        <h1>Faites un Quiz en ligne !</h1></br></br>
+        <h2>Résultats sur le  : <?= $quiz->getNom(); ?></h2>
       </header>
 
       <div class="panel panel-default">
         <div class="panel-body">
-        <h2>Merci d'avoir effectué un quiz !</h2>
+        <h2>Merci d'avoir effectué ce quiz !</h2>
         <p>Voici la correction : </p>
         <main>
-        <?php
-          // Affichage des questions grâce à une boucle foreach
-          foreach ($bonnes_reponses as $br)
+          <?php
+          // Boucle qui sélectionne les quizs un par un, mais il n'y en a qu'un
+          foreach ($quizs as $quiz)
           {
-            $j++;
-            $obj_br = new BReponse($br)
+              // Affichage des questions grâce à une boucle foreach
+              foreach ($quiz->getQuestions() as $question)
+              {
+                $j ++;
+                // Condition pour savoir quel est le type de la question
+                if ($question->getType() == 1) 
+                {
+                ?>
+                <h4 style="color: rgba(147,70,23);"> Question n°<?= $j; ?> : <?= $question->getQuestion(); ?> </h4><br/>
+                <?php
+                foreach ($question->getReponses() as $rep) 
+                {
+                  // Fonction remplissage : Vérifie que tous les formulaires soient remplis. Dans le cas contraire, arrête le code et redirige vers le quiz.
+                  remplissage($_POST['reponse'][$j], $_GET['id_quiz']);
+                  // Condition pour vérifier si la réponse est juste
+                  if ($_POST['reponse'][$j] == $rep->getReponse()) 
+                  { 
+                    echo 'Vous avez eu juste !<br/>';
+                    echo 'Bonne réponse : <strong>' . $rep->getReponse() . '</strong>';
+                    echo "<br/>";
+                    // Incrémentation du score si la réponse est juste
+                    $score ++;
+                  }
+                  else
+                  {
+                    echo 'Vous vous êtes tromper !<br/>';
+                    echo 'La bonne réponse était : <strong>' . $rep->getReponse() . '</strong>';
+                    echo "<br/>";
+                  }
+                }
+                }
+                else
+                {
+                ?>
+                  <h4 style="color: rgba(147,70,23);"> Question n°<?= $j; ?> : <?= $question->getQuestion(); ?> </h4><br/>
+                  <?php
+                  $reponses_fausses = 0;
+                  $bonnes_reponses = [];
+                  foreach ($question->getReponses() as $reponse) 
+                  {
+                    // Encore la même fonction remplissage
+                    remplissage($_POST['reponse'][$reponse->getReponse()], $_GET['id_quiz']);
+                    // Création d'un tableau bonnes_réponses pour stocker les bonnes réponses
+                    $bonnes_reponses[$reponse->getId()] = $reponse->getReponse();
+                    // Si une seule bonne réponse n'a pas été coché alors l'utilisateur a faux à toute la question
+                    if (!isset($_POST['reponse'][$reponse->getReponse()]))
+                    { 
+                      $reponses_fausses ++;
+                    }                                  
+                  }
+                  if ($reponses_fausses == 0) 
+                  {
+                    echo 'Vous avez eu juste !<br/>';
+                    echo 'Bonnes réponses : <strong>';
+                    // Boucle qui affiche toutes les bonnes réponses
+                    foreach ($bonnes_reponses as $br){ echo $br . ' '; }
+                    echo '</strong>';
+                    echo "<br/>";
+                    $score ++;
+                  }
+                  else
+                  {
+                    echo 'Vous vous êtes tromper !<br/>';
+                    echo 'Les bonnes réponses étaient : <strong>';
+                    // Boucle qui affiche toutes les bonnes réponses
+                    foreach ($bonnes_reponses as $br){ echo $br . ' '; }
+                    echo '</strong>';
+                    echo "<br/>";
+                  }
+                }                                            
+              }
+          }
+          // Attribution d'une valeur à $_SESSION['Quiz_done'][$_GET['id_quiz']] pour éviter que l'utilisateur refasse le même quiz 2 fois (voir fonction Is_autorized)
+          $_SESSION['Quiz_done'][$_GET['id_quiz']]=$_GET['id_quiz'];
           ?>
             <br/>
-            <h4 style="color: rgba(147,70,23);"> Question n°<?= $j; ?> : <?= $obj_br->question(); ?> </h4><br/>
-          <?php
-          // Je ne sais pas quoi mettre entre parenthèse dans la variable $_POST pour récupérer à chaque fois la réponse $_POST['1'], $_POST['2'], ...
-          if ($_POST['reponse'][$j] == $obj_br->brep()) 
-          {
-            echo 'Vous avez eu juste !<br/>';
-            echo 'Bonne réponse : <strong>' . $obj_br->brep() . '</strong>';
-            echo "<br/>";
-            $score ++;
-
-          }
-          else
-          {
-            echo 'Vous vous êtes tromper !<br/>';
-            echo 'La bonne réponse était : <strong>' . $obj_br->brep() . '</strong>';
-            echo "<br/>";
-          }
-          }
-        ?>
-        <br/>
-        <p style="font-size: 1.5em";>Votre score est de : <strong style="color: red";><?= ($score / 8)*100 ?> % de réussite</strong></p><br/>
+            <p style="font-size: 1.5em";>Votre score est de : <strong style="color: red";><?= ceil(($score / $j)*100) ?> % de réussite</strong></p>
+            <br/>
         </main>
         <a href="?controller=accueil_quiz">Retour au sommaire</a><br/>
         </div>
       </div>
       <footer class="well">
-                Tout droits réservés
+        Tout droits réservés
       </footer>
   </div>
-  </body> 
+  </body>
