a483c59759a256556985bb3d256351412e7a5e21
raghast <abardy26@gmail.com>
2017-06-09 19:08:46 +0200

    Changement affichage_resultats - Historique + fonctions
    Changement et rajout de quelques fonctions (check_variables, range_tableau ..)
    Rajout de toute une boucle foreach (avec plusieurs boucle à l'intérieur) pour récupérer les réponses de l'utilisateur et les mettre dans un tableau historique (qui sera ensuite sérialisé et ranger en BDD)

--- Controller/affichage_resultat.php
+++ Controller/affichage_resultat.php
@@ -7,25 +7,68 @@ require_once 'Modele/fonctions_quiz.php';
 require_once 'Modele/reponse.php';
 
 
-// Fonctions de vérification de la page (pour la variable $_GET['id_quiz'], et Is_autorized pour savoir si l'utilisateur à déjà fais le quiz)
-check_variable();
-check_id($_GET['id_quiz']);
-Is_autorized($_GET['id_quiz']);
+// Fonction de vérification de la page (pour la variable $_GET['id_quiz'], $_GET['nom_quiz']
+check_variables();
 
 // Même système que pour le controller page_quiz mais importation_br importe un quiz, ses questions et la bonne réponse pour chaque question
-$datas = importation_br($_GET['id_quiz']);
-$quizs = [];
-foreach ($datas as $data) 
+$bonnes_rep = importation_br($_GET['id_quiz']);
+// On importe aussi tout le quiz dans une autre variable pour l'étape d'aprés
+$quiz_all = importation_tout($_GET['id_quiz']);
+$all_rep = [];
+$quiz_br = [];
+$historique = [];
+$j = 0;
+
+// Fonction pour créer les tableaux d'objets
+$quiz_br = range_tableau($bonnes_rep, $quiz_br);
+$all_rep = range_tableau($quiz_all, $all_rep);
+
+foreach ($all_rep as $quizz)
 {
-	$quiz = new Quiz($data);
-	if (isset($quizs[$quiz->getId()])) 
-	{
-       $quizs[$quiz->getId()]->fusion($quiz);
-    } else 
+	// Création du tableau ou l'on stocké les réponses de l'utilisateur à mettre en BDD
+	$historique[$quizz->getId()] = [];
+    // Affichage des questions grâce à une boucle foreach
+    foreach ($quizz->getQuestions() as $question)
     {
-       $quizs[$quiz->getId()] = $quiz;
+    	//Création d'un tableau d'ID des questions dans le tableau précédent
+     	$historique[$quizz->getId()][$question->getId()] = [];
+     	$j ++;         
+        // Condition pour savoir quel est le type de la question
+        if ($question->getType() == 1 AND isset($_POST['reponse'][$j])) 
+        {   
+        	// On assigne les réponses de l'utilisateur à l'ID de question correspondante   
+            $historique[$quizz->getId()][$question->getId()][$j] = $_POST['reponse'][$j];      
+        }
+    	elseif ($question->getType() == 2) 
+        {
+            foreach ($question->getReponses() as $reponse) 
+            {
+                if (isset($_POST['reponse'][$reponse->getReponse()]))
+                {
+                	$historique[$quizz->getId()][$question->getId()][$reponse->getId()] = $reponse->getReponse();
+                }                           
+            }          
+        }
+        elseif ($question->getType() == 3 AND isset($_POST['nombre'][$j])) 
+        {
+        	$historique[$quizz->getId()][$question->getId()][$j] = htmlspecialchars($_POST['nombre'][$j]);           
+        }
+        else
+        {   
+        	foreach ($question->getReponses() as $reponse) 
+        	{                  
+                if (isset($_POST['reponse'][$reponse->getReponse()])) 
+                {
+                	// Pareil qu'au dessus mais on rajoute l'ordre choisi devant la réponse
+                    $historique[$quizz->getId()][$question->getId()][$reponse->getId()] = htmlspecialchars($_POST['reponse'][$reponse->getReponse()]) . ' - ' . $reponse->getReponse();
+                }                
+            }                                            
+        }
     }
 }
+//Préparation de la variable de session et du tableau des réponses sérialisées à stocker en BDD
+$session_id = session_id();
+$hist_serial = serialize($historique);
 
 // Initialisation de la numérotation des questions
 $j=0;
