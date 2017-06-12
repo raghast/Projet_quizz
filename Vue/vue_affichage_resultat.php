68a516942c9b92307c0d078aa00f48d6929f88f4
raghast <abardy26@gmail.com>
2017-06-12 19:29:03 +0200

    Modification vue_affichage_résultat
    Utilisation de fonctions pour alléger le code - Voir Modèle/Fonctions_quiz

--- Vue/vue_affichage_resultat.php
+++ Vue/vue_affichage_resultat.php
@@ -18,98 +18,105 @@
   <div class="container">
       <header class="jumbotron">
         <h1>Faites un Quiz en ligne !</h1></br></br>
-        <h2>Résultats sur le  : <?= $quiz->getNom(); ?></h2>
+        <h2>Résultats sur le  : <?= $_GET['nom_quiz'];?></h2>
       </header>
 
       <div class="panel panel-default">
         <div class="panel-body">
-        <h2>Merci d'avoir effectué ce quiz !</h2>
-        <p>Voici la correction : </p>
-        <main>
+          <h2>Merci d'avoir effectué ce quiz !</h2>
+          <p>Voici la correction : </p>
+          <p>Les réponses en orange sont les bonnes réponses.</p>
+          <main>
           <?php
           // Boucle qui sélectionne les quizs un par un, mais il n'y en a qu'un
-          foreach ($quizs as $quiz)
-          {
+          foreach ($all_rep as $quiz)
+          {             
               // Affichage des questions grâce à une boucle foreach
               foreach ($quiz->getQuestions() as $question)
-              {
+              {              
                 $j ++;
-                // Condition pour savoir quel est le type de la question
-                if ($question->getType() == 1) 
-                {
                 ?>
                 <h4 style="color: rgba(147,70,23);"> Question n°<?= $j; ?> : <?= $question->getQuestion(); ?> </h4><br/>
                 <?php
-                foreach ($question->getReponses() as $rep) 
-                {                 
-                  // Condition pour voir si une réponse a été sélectionnée
-                  if (!isset($_POST['reponse'][$j])) 
+                // Condition pour savoir quel est le type de la question
+                if ($question->getType() == 1) 
+                {
+                  foreach ($question->getReponses() as $rep) 
                   {
-                    echo 'Vous vous êtes tromper !<br/>';
-                    echo 'La bonne réponse était : <strong>' . $rep->getReponse() . '</strong>';
-                    echo "<br/>";
-                  }
-                  // Condition pour vérifier si la réponse est juste
-                  elseif ($_POST['reponse'][$j] == $rep->getReponse()) 
+                    if (!isset($_POST['reponse'][$j])) 
+                    {
+                        // Reponse fausse -> $message = 0
+                        $message = 0;
+                    }
+                    elseif ($$_POST['reponse'][$j] == $rep->getReponse() AND $rep->getState() == 1) 
+                    { 
+                        // Reponse juste -> $message = 1
+                        $message = 1;
+                    }
+                    else
+                    {
+                    // Reponse fausse -> $message = 0
+                    $message = 0;
+                    }
+                  }          
+                  $score = affichage_message($message, $score);
+                  affichage_reponses($question);
+                }
+                elseif ($question->getType() == 2) 
+                {
+                  $message = 1; 
+                  foreach ($question->getReponses() as $rep) 
+                  {  
+                      // Si une seule bonne réponse n'a pas été coché ou une question fausse a été coché alors l'utilisateur a faux à toute la question
+                      if (!isset($_POST['reponse'][$reponse->getReponse()]) AND $rep->getState() == 1)
+                      { 
+                          $message = 0;
+                      }
+                      elseif (isset($_POST['reponse'][$reponse->getReponse()]) AND $rep->getState() == 0) 
+                      {
+                          $message = 0;
+                      }
+                  }                              
+                  $score = affichage_message($message, $score);
+                  affichage_reponses($question);
+                }
+                elseif ($question->getType() == 3) 
+                {
+                  $message = 1;
+                  if ($_POST['nombre'][$j] == $rep->getReponse() AND $rep->getState() == 1) 
                   { 
-                    echo 'Vous avez eu juste !<br/>';
-                    echo 'Bonne réponse : <strong>' . $rep->getReponse() . '</strong>';
-                    echo "<br/>";
-                    // Incrémentation du score si la réponse est juste
-                    $score ++;
+                      // Reponse juste -> $message = 1
+                      $message = 1;
                   }
                   else
                   {
-                    echo 'Vous vous êtes tromper !<br/>';
-                    echo 'La bonne réponse était : <strong>' . $rep->getReponse() . '</strong>';
-                    echo "<br/>";
-                }
+                      // Reponse fausse -> $message = 0
+                      $message = 0;
+                  }                              
+                  $score = affichage_message($message, $score);
+                  affichage_reponses($question);
                 }
                 else
-                {
-                ?>
-                  <h4 style="color: rgba(147,70,23);"> Question n°<?= $j; ?> : <?= $question->getQuestion(); ?> </h4><br/>
-                  <?php
-                  $reponses_fausses = 0;
-                  $bonnes_reponses = [];
-                  foreach ($question->getReponses() as $reponse) 
-                  {                    
-                    // Création d'un tableau bonnes_réponses pour stocker les bonnes réponses
-                    $bonnes_reponses[$reponse->getId()] = $reponse->getReponse();
-                    // Si une seule bonne réponse n'a pas été coché alors l'utilisateur a faux à toute la question
-                    if (!isset($_POST['reponse'][$reponse->getReponse()]))
-                    { 
-                      $reponses_fausses ++;
-                    }                                  
-                  }
-                  if ($reponses_fausses == 0) 
+                { 
+                  $message = 1;
+                  foreach ($question->getReponses() as $rep) 
                   {
-                    echo 'Vous avez eu juste !<br/>';
-                    echo 'Bonnes réponses : <strong>';
-                    // Boucle qui affiche toutes les bonnes réponses
-                    foreach ($bonnes_reponses as $br){ echo $br . ' '; }
-                    echo '</strong>';
-                    echo "<br/>";
-                    $score ++;
+                    // Si un ordre ne correspond pas alors toute la question est fausse                    
+                    if (!($rep->getState() == $_POST['reponse'][$rep->getReponse()])) 
+                    {
+                        $message = 0;
+                    }
                   }
-                  else
-                  {
-                    echo 'Vous vous êtes tromper !<br/>';
-                    echo 'Les bonnes réponses étaient : <strong>';
-                    // Boucle qui affiche toutes les bonnes réponses
-                    foreach ($bonnes_reponses as $br){ echo $br . ' '; }
-                    echo '</strong>';
-                    echo "<br/>";
-                  }
+                  $score = affichage_message($message, $score);
+                  affichage_reponses_4($question);                                               
                 }                                            
               }
           }
-          // Attribution d'une valeur à $_SESSION['Quiz_done'][$_GET['id_quiz']] pour éviter que l'utilisateur refasse le même quiz 2 fois (voir fonction Is_autorized)
-          $_SESSION['Quiz_done'][$_GET['id_quiz']]=$_GET['id_quiz'];
+          $score_calcul = ceil(($score / $j)*100);
+          range_hist_bdd($hist_serial, $score_calcul, $session_id);
           ?>
-            <br/>
-            <p style="font-size: 1.5em";>Votre score est de : <strong style="color: red";><?= ceil(($score / $j)*100) ?> % de réussite</strong></p>
-            <br/>
+          <p style="font-size: 1.5em";>Votre score est de : <strong style="color: red";><?= $score_calcul ?> % de réussite</strong></p>
+          <br/>
         </main>
         <a href="?controller=accueil_quiz">Retour au sommaire</a><br/>
         </div>
