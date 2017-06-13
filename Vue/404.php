c3b80ad2eb0c097bcb47da09a76af4c8eeea60be
raghast <abardy26@gmail.com>
2017-06-13 17:45:45 +0200

    Ajout de la vue Error 404.php
    Ajout de la vue 404.php ! J'avais oublié de la mettre !

---
+++ Vue/404.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blog d'Aurélien</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+              crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+              integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+              crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="container">
+            <header class="jumbotron">
+                <h1>Blog d'Aurélien !</h1>
+                <h2>Error 404</h2>
+            </header>
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <main>
+                        La page n'existe pas.
+                    </main>
+                    <a href="?controller=accueil_quiz">Retour au sommaire</a>
+                </div>
+            </div>
+
+            <footer class="well">
+                Tout droits réservés
+            </footer>
+        </div>
+    </body>
+</html>
